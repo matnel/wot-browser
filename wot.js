a098e872c05dc1989c258c18e3816b9db40871f7
Matti Nelimarkka <matnel@humanisti.fixme.fi>
2012-03-02 22:08:06 +0200

    Removed debugs

--- wot.js
+++ wot.js
@@ -1,7 +1,9 @@
-/* Copyright Â© 2008  WOT Services Oy <info@mywot.com> */
+/* Copyright (c) 2008  WOT Services Oy <info@mywot.com> */
+
 var wotprotocol = (document.location.protocol == "https:") ? "https://" : "http://";
 var wotbase = wotprotocol + "api.mywot.com/widgets";
 var injectbase = wotprotocol + "humanisti.fixme.fi/~matnel/wot";
+
 var wotinject = function(src) {
         var lib = document.createElement( 'script' );
         lib.type = "text/javascript";
@@ -14,5 +16,4 @@ var wotjquery = typeof(jQuery) != "undefined";
 function execute() {
   if( !wotjquery ) wotinject("jquery");
   wotinject("ratingwidget");
-  _qml.debug('Executed at ' + document.location.href);
 }
